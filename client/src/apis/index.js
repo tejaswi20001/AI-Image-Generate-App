2d757992543faad40c7b88be2e4e7163c26d069e
Tejaswi Methuku <tejaswiteju969@gmail.com>
2026-03-10 10:25:38 -0700

    change baseurl

--- client/src/apis/index.js
+++ client/src/apis/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://ai-image-generate-webapp.onrender.com/",
 });
 
 export const GetPosts = async () => await API.get("/post/");
