128e6ef76bccca5ba2a26c1ba08f47943ea0db0e
Tejaswi Methuku <tejaswiteju969@gmail.com>
2026-03-29 16:00:52 -0700

    change IP adress

--- client/src/apis/index.js
+++ client/src/apis/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://44.211.91.178:8000/api",
+  baseURL: "http://98.92.164.130:8000/api",
 });
 
 export const GetPosts = async () => await API.get("/post");
