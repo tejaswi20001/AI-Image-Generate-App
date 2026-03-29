f33d368244c649e1bf6d8098a3839e878fabb71b
Tejaswi Methuku <tejaswiteju969@gmail.com>
2026-03-29 14:47:08 -0700

    check apis

--- server/index.js
+++ server/index.js
@@ -11,7 +11,11 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
